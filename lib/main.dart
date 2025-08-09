dba31e8df9ab1635f1d8da34115b985470186aba
Mikel <kelikolaveri@gmail.com>
2025-08-09 04:01:51 +0200

    remove useless import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:recipe_app/pages/landing_page.dart';
 import 'package:recipe_app/router/router.dart';
 
 void main() {
